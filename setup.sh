8966cadff1c762045c0d255fe54444d52f8f012a
Pete Shima <pete.shima@epicgames.com>
2022-07-27 15:19:54 -0700

    Remove some setup steps

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install git
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
 
-sudo apt-get install -y uidmap
-dockerd-rootless-setuptool.sh install
-sudo usermod -aG docker admin
-sudo apt-get install python3-pip
+
+
 
 echo "export PATH=/usr/bin:$PATH" >> ~/.bashrc
 echo "export DOCKER_HOST=unix:///run/user/1000/docker.sock" >> ~/.bashrc
